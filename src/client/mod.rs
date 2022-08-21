58bf6084c4e504a225cf563e1a00089b92cd3870
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2022-08-22 01:27:08 +0300

    encapsulate implementation details into inner and remove access to impl details

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -26,16 +26,32 @@ use std::sync::Arc;
 use async_std::channel::{Receiver,Sender,unbounded};
 use workflow_core::channel::oneshot;
 
+struct Inner {
+    client : Arc<WebSocketInterface>,
+    sender_tx : Sender<DispatchMessage>,
+    receiver_rx : Receiver<Message>,
+}
+
+impl Inner {
+    pub fn new(
+        client: Arc<WebSocketInterface>,
+        sender_tx: Sender<DispatchMessage>,
+        receiver_rx: Receiver<Message>,
+    ) -> Self {
+        Self {
+            client,
+            sender_tx,
+            receiver_rx,
+        }
+    }
+}
+
 #[derive(Clone)] 
 pub struct WebSocket {
-    client : Arc<WebSocketInterface>,
-    pub sender_tx : Sender<DispatchMessage>,
-    pub receiver_rx : Receiver<Message>,
-    pub receiver_tx : Sender<Message>,
+    inner: Arc<Inner>,
 }
 
 impl WebSocket {
-    // pub fn new(url : &str, settings : Settings) -> Result<Arc<WebSocket>> {
     pub fn new(url : &str) -> Result<WebSocket> {
 
         let (receiver_tx, receiver_rx) = unbounded::<Message>();
@@ -47,52 +63,91 @@ impl WebSocket {
 
         let client = Arc::new(WebSocketInterface::new(
             url,
-            receiver_tx.clone(),
+            receiver_tx,
             sender_tx_rx
         )?);
 
-        let websocket = WebSocket {
-            client,
-            sender_tx,
-            receiver_rx,
-            receiver_tx,
+        let websocket = WebSocket { 
+            inner : Arc::new(Inner::new(
+                client, sender_tx, receiver_rx
+            )) 
         };
 
         Ok(websocket)
     }
 
+    ///
+    /// Returns reference to the transmission channel.
+    /// You can clone the channel and use it directly.
+    /// The channel sends DispatchMessage struct that
+    /// has two values:
+    /// 
+    ///     - DispatchMessage::Post(Message)
+    ///     - DispatchMessage::WithAck(Message, Sender<Result<Arc<(),Arc<Error>>>)
+    /// 
+    /// To use WithAck message, you need to create an instance
+    /// of `oneshot` channel and supply the sender, while retain
+    /// and await on receiver.recv() to get acknowledgement that
+    /// the message has been successfully handed off to the underlying
+    /// websocket.
+    /// 
+    pub fn sender_tx<'ws>(&'ws self) -> &'ws Sender<DispatchMessage> {
+        &self.inner.sender_tx
+    }
+
+    /// Returns the reference to the receiver channel
+    pub fn receiver_rx<'ws>(&'ws self) -> &'ws Receiver<Message> {
+        &self.inner.receiver_rx
+    }
+
+    /// Connects the websocket to the destination URL.
+    /// Optionally accepts `block_until_connected` argument
+    /// that will block the async execution until the websocket
+    /// is connected.
     pub async fn connect(&self, block_until_connected : bool) -> Result<()> {
-        Ok(self.client.connect(block_until_connected).await?)
+        Ok(self.inner.client.connect(block_until_connected).await?)
     }
 
+    /// Disconnects the websocket from the destination server.
     pub async fn disconnect(&self) -> Result<()> {
-        Ok(self.client.disconnect().await?)
+        Ok(self.inner.client.disconnect().await?)
     }
 
+    /// Sends a message to the destination server. This function
+    /// will queue the message on the relay channel and return
+    /// successfully if the message has been queued.
+    /// This function enforces async yield in order to prevent
+    /// potential blockage of the executor if it is being executed
+    /// in tight loops.
     pub async fn post(&self, message: Message) -> Result<&Self> {
-        if !self.client.is_open() {
+        if !self.inner.client.is_open() {
             return Err(Error::NotConnected);
         }
         
-        let result = Ok(self.sender_tx.send(DispatchMessage::Post(message)).await?);
+        let result = Ok(self.inner.sender_tx.send(DispatchMessage::Post(message)).await?);
         workflow_core::task::yield_now().await;
         result.map(|_| self)
     }
 
+    /// Sends a message to the destination server. This function
+    /// will block until until the message was relayed to the
+    /// underlying websocket implementation.
     pub async fn send(&self, message: Message) -> std::result::Result<&Self,Arc<Error>> {
-        if !self.client.is_open() {
+        if !self.inner.client.is_open() {
             return Err(Arc::new(Error::NotConnected));
         }
         
         let (sender, receiver) = oneshot(); 
-        self.sender_tx.send(DispatchMessage::WithAck(message, sender)).await
+        self.inner.sender_tx.send(DispatchMessage::WithAck(message, sender)).await
             .map_err(|err|Arc::new(err.into()))?;
     
         receiver.recv().await.map_err(|_|Arc::new(Error::DispatchChannelAck))?
             .map(|_|self)
     }
 
+    /// Receives message from the websocket. Blocks until a message is
+    /// received from the underlying websocket connection.
     pub async fn recv(&self) -> Result<Message> {
-        Ok(self.receiver_rx.recv().await?)
+        Ok(self.inner.receiver_rx.recv().await?)
     }
 }
